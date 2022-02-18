8c9a1456da6b845393668eea5ea50b1fe45a9108
Trendon Cato <trendoncato@hotmail.com>
2022-02-19 00:51:31 +0800

    Add all services to docker-compose

--- Shopping/Shopping.API/Startup.cs
+++ Shopping/Shopping.API/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Shopping.API.Data.Shopping.API.Data;
 
 namespace Shopping.API
 {
@@ -32,6 +26,8 @@ namespace Shopping.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shopping.API", Version = "v1" });
             });
+
+            services.AddScoped<ProductContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Shopping/Shopping.Client/Startup.cs
+++ Shopping/Shopping.Client/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Shopping.Client
 {
@@ -23,7 +19,13 @@ namespace Shopping.Client
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddHttpClient("ShoppingAPIClient", client =>
+            {
+                //client.BaseAddress = new Uri("http://localhost:5000/"); // Shopping.API url     
+                client.BaseAddress = new Uri(Configuration["ShoppingAPIUrl"]);
+            });
+
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
